230142bfd085b4381e3bbf0ebdbc325b5f4cd3f7
Thanh Tran <thanh.d.tran@zamo.io>
2026-05-25 13:08:24 +0700

    Setup api for login feature

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,11 @@
 import bcrypt from "bcrypt";
 import { User } from "../models/User.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import { Session } from "../models/Session.js";
+
+const ACCESS_TOKEN_TTL = "30m";
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000; // 14 days
 
 export const register = async (req, res) => {
   try {
@@ -23,6 +29,52 @@ export const register = async (req, res) => {
     });
 
     return res.sendStatus(204);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password)
+      return res
+        .status(400)
+        .json({ message: "Username and password are required" });
+
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    const passwordCorrect = await bcrypt.compare(password, user.hashedPassword);
+
+    if (!passwordCorrect)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: ACCESS_TOKEN_TTL },
+    );
+
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+
+    return res.status(200).json({ accessToken });
   } catch (e) {
     console.error(e);
     return res.status(500).json({ message: "Internal server error" });
