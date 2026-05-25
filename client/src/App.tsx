8d451f72ac40be4b88470f94c555686d5aef8e14
Thanh Tran <thanh.d.tran@zamo.io>
2026-05-25 14:15:35 +0700

    Create 3 new pages and setup routes for these pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import { LoginPage } from "./pages/login.page";
+import { RegisterPage } from "./pages/register.page";
+import { ChatPage } from "./pages/chat.page";
+import { Toaster } from "sonner";
+
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
